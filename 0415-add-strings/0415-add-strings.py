09b100a91101461fc82e781f9fd0ed1ee3ebebc3
prabath17 <136706003+prabath17@users.noreply.github.com>
2025-05-26 18:44:02 +0530

    Time: 19 ms (11.84%), Space: 12.6 MB (16.81%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        a=0
+        b=0
+        for i in num1:
+            a=a*10 +(ord(i)-48)
+        for i in num2:
+            b=b*10 +(ord(i)-48)
+        return str(a+b)
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
