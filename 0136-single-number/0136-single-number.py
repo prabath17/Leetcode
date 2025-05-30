6df413b294e57b75bd1b77ff6fda4c717a9fa562
prabath17 <136706003+prabath17@users.noreply.github.com>
2025-05-30 20:56:32 +0530

    Time: 10 ms (36.47%), Space: 15.2 MB (6.69%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        freq={}
+        for i in nums:
+            freq[i]=freq.get(i,0)+1
+        for key, value in freq.items():
+            if value==1:
+                return key
+        
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
